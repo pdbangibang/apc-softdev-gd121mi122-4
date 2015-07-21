e06ebe52074c61259c5002da337929fb5a2c32f7
lsamonte <lesliemsamonte@gmail.com>
2015-07-21 13:17:03 +0800

    Created By Leslie Samonte

--- practice/midterm_test/Samonte_Leslie_midterms/ourspace/views/myaddress/index.php
+++ practice/midterm_test/Samonte_Leslie_midterms/ourspace/views/myaddress/index.php
@@ -1,39 +1,9 @@
 <?php
-
-use yii\helpers\Html;
-use yii\grid\GridView;
-
 /* @var $this yii\web\View */
-/* @var $searchModel app\models\MyaddressSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
-
-$this->title = 'Myaddresses';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="myaddress-index">
+<h1>myaddress/index</h1>
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <p>
-        <?= Html::a('Create Myaddress', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'firstname',
-            'middlename',
-            'lastname',
-            'gender',
-            // 'created_at',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-
-</div>
+<p>
+    You may change the content of this page by modifying
+    the file <code><?= __FILE__; ?></code>.
+</p>
