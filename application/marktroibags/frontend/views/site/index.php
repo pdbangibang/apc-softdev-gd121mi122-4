eb4dc60570abe58619aeb33175394134ea929236
lsamonte <lesliemsamonte@gmail.com>
2015-07-30 16:29:18 +0800

    Modified By Leslie Samonte

--- application/marktroibags/frontend/views/site/index.php
+++ application/marktroibags/frontend/views/site/index.php
@@ -12,13 +12,15 @@ $this->title = 'Makrtroi Bags';
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         
         <!-- stylesheets -->
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="assets/font_icon/css/pe-icon-7-stroke.css">
         <link rel="stylesheet" href="assets/font_icon/css/helper.css">
         <link rel="stylesheet" href="assets/css/owl.carousel.css">
         <link rel="stylesheet" href="assets/css/owl.theme.css">
         <link rel="stylesheet" href="assets/css/animate.css">
         <link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
 
         <!-- google fonts -->
         <link href='http://fonts.googleapis.com/css?family=Dosis:200,300,400,500|Lato:300,400,700,900,300italic,400italic,700italic,900italic|Raleway:400,200,300,500,100|Titillium+Web:400,200,300italic,300,200italic' rel='stylesheet' type='text/css'>
