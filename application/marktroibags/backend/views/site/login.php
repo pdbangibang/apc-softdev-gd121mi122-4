1a27984037ba9f29cd36a795696c33c97a1e868e
lsamonte <lesliemsamonte@gmail.com>
2015-08-14 07:59:01 +0800

    Updated By Leslie Samonte

--- application/marktroibags/backend/views/site/login.php
+++ application/marktroibags/backend/views/site/login.php
@@ -1,6 +1,7 @@
 <?php
-use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
+
+use kartik\helpers\Html;
+use kartik\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
@@ -60,39 +61,20 @@ $this->title = 'Login';
             <div id="login" class="animate form">
                 <section class="login_content">
 
-                    
-                        <h1>Login Form</h1>
-                        <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-							<?= $form->field($model, 'username') ?>
-							<?= $form->field($model, 'password')->passwordInput() ?>
-							<?= $form->field($model, 'rememberMe')->checkbox() ?>
-						<div style="color:#999;margin:1em 0">
-							If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-						</div>
-						<div class="form-group">
-						<?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-
-            <?php ActiveForm::end(); ?>
-                    <!-- form -->
-                </section>
-                <!-- content -->
-            </div>
-            <div id="register" class="animate form">
-                <section class="login_content">
-
-                    <form>
-                        <h1>Login Form</h1>
-                         <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-                <?= $form->field($model, 'username') ?>
-                <?= $form->field($model, 'password')->passwordInput() ?>
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-                <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-                </div>
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-            <?php ActiveForm::end(); ?>
+                   <?php 
+    $form = ActiveForm::begin([
+        'id' => 'login-form-vertical', 
+        'type' => ActiveForm::TYPE_VERTICAL
+    ]); 
+?>
+    <?= $form->field($model, 'username') ?>
+    <?= $form->field($model, 'password')->passwordInput() ?>
+    <?= $form->field($model, 'rememberMe')->checkbox() ?>
+    <div class="form-group">
+        <?= Html::submitButton('Login', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+<?php ActiveForm::end(); ?>
                             <div class="clearfix"></div>
                             <br />
                             <div>
